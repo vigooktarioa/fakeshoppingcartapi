51c9ca0cfb75353f3f053637b5312d74d79e4ca8
Laplace <vigooktarioa@gmail.com>
2022-08-14 09:42:28 +0700

    error sort by category

--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -1,11 +1,41 @@
 import React, { Component} from 'react';
-import { Col } from 'react-bootstrap'
+import { Col, ListGroup } from 'react-bootstrap'
+import axios from 'axios'
+import { API_URL } from '../utils/constants';
 
 export default class ListCategories extends Component{
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         categories : []
+      }
+    }
+
+    componentDidMount(){
+        axios
+        .get(API_URL + "/products/categories")
+        .then(res => {
+          const categories = res.data;
+          this.setState({ categories });
+        })
+        .catch(error => {
+          console.log(error);
+        })
+      
+    }
+    
     render(){
+        const {categories} = this.state
     return(
         <Col md={2} mt="2">
             <h4><strong>Daftar Kategori</strong></h4>
+            <hr/>
+            <ListGroup>
+                {categories && categories.map((category) => (
+                    <ListGroup.Item>{category}</ListGroup.Item>
+                ))}
+      </ListGroup>
         </Col>
 
         
--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Card, Col, Button } from 'react-bootstrap'
+import '../App.css'
 
 const Menus = ({menu}) => {
   return (
-    <div>
-        <h2>{menu.id}</h2>
-    </div>
+    <Col md={4} xs={6} className="mb-4">
+    <Card style={{ width: '18rem' }} className="card-menu">
+      <Card.Img variant="top" src={menu.image} className="img-card"/>
+      <Card.Body>
+        <Card.Title>{menu.title}</Card.Title>
+        <Card.Text>${menu.price}</Card.Text>
+      </Card.Body>
+    </Card>
+    </Col>
   )
 }
 
